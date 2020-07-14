4024057f8dedb389615ce590c74efee1b65f1d4e
DIMEGLIO Théo <theo.dimeglio@niji.fr>
2020-07-14 17:49:52 +0200

    change uuid to id and return drink on put update

--- api/drinks_service.py
+++ api/drinks_service.py
@@ -8,11 +8,11 @@ def list():
         data = json.load(json_file)
         return data
 
-def get(uuid):
+def get(id):
     with open(JSON_FILE_PATH) as json_file:
         data = json.load(json_file)
         for drink in data:
-            if drink['uuid'] == uuid: return drink
+            if drink['id'] == id: return drink
         return None
 
 def add(new_drink):
@@ -22,30 +22,32 @@ def add(new_drink):
             if drink['pump_number'] == new_drink['pump_number'] : return False
         file.seek(0)
         file.truncate()
-        new_drink['uuid'] = util.get_uuid()
+        new_drink['id'] = util.get_uuid()
         data.append(new_drink)
         json.dump(data, file)
-        return True
+        for drink in data:
+            if drink['id'] == new_drink['id']: return drink
 
-def delete(uuid):
+def delete(id):
     with open(JSON_FILE_PATH, "r+") as file:
         data = json.load(file)
         for index, drink in enumerate(data):
-            if drink['uuid'] == uuid : del data[index]
+            if drink['id'] == id : del data[index]
         file.seek(0)
         file.truncate()
         json.dump(data, file)
         return True
 
-def modify(uuid, new_value):
+def modify(id, new_value):
     with open(JSON_FILE_PATH, "r+") as file:
         data = json.load(file)
         for index, drink in enumerate(data):
-            if drink['uuid'] == uuid : 
+            if drink['id'] == id : 
                 for new_val in new_value.keys():
                     print(new_val)
                     data[index][new_val] = new_value[new_val]
         file.seek(0)
         file.truncate()
         json.dump(data, file)
-        return True
+        for drink in data:
+            if drink['id'] == id: return drink
